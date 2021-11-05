08c90178ea3e437439f5f7dfef07642097475a95
Joel Granberg <granberg.joel@gmail.com>
2021-11-05 11:40:13 +0100

    removed old files

--- frontend-tests/cypress/integration/playgroiund.spec.js
+++ frontend-tests/cypress/integration/playgroiund.spec.js
@@ -22,38 +22,7 @@ afterEach(()=>{
     dashboardFuncs.performLogout(cy, 'Login')
 }) 
 
-it('T07 - change reservation', function(){
-    indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
-    dashboardFuncs.navigateToViewReservation(cy, 'Reservations') 
-    reservationsFuncs.checkTitleReservationsPage(cy)
-    
-    //Assert original data is correct
-    reservationsFuncs.assertNewData(cy, '2020-04-01', '2020-04-04')//Move to targets?
-    reservationsFuncs.openMenuExistingReservation(cy)
-    reservationsFuncs.clickEditExistingReservation(cy)
-    editReservationPage.checkTitleEditPage(cy)
-    editReservationPage.clearStartTextfield(cy)
-    editReservationPage.enterNewStart(cy, '2021-12-12') //Move to targets?
-    editReservationPage.clearEndTextfield(cy)
-    editReservationPage.enterNewEnd(cy, '2021-12-13') //Move to targets?
-    editReservationPage.saveReservation(cy)
-    reservationsFuncs.checkTitleReservationsPage(cy)
 
-    //Assert new data is correct
-    reservationsFuncs.assertNewData(cy, '2021-12-12', '2021-12-13') //move to targets?
-    reservationsFuncs.openMenuExistingReservation(cy)
-    reservationsFuncs.clickEditExistingReservation(cy)
-    editReservationPage.checkTitleEditPage(cy)   
-    editReservationPage.clearStartTextfield(cy)
-    editReservationPage.enterOldStart(cy, '2020-04-01')//Move to targets?
-    editReservationPage.clearEndTextfield(cy)
-    editReservationPage.enterOldEnd(cy, '2020-04-04') //Move to targets?
-    editReservationPage.saveReservation(cy)
-
-    reservationsFuncs.assertNewData(cy, '2020-04-01', '2020-04-04') //Move to targets?
-
-    reservationsFuncs.checkTitleReservationsPage(cy)
-})
 
 
 
